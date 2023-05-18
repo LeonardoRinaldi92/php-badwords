56b3f583e0774c98b1a6c6cb4765c1abb6a1070c
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-18 16:11:53 +0200

    struttura 2

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <form action="risposta.php" method="GET">
+        <div style = "width: 30%; display : flex; flex-direction: column">
+            <label for="parola">parola</label>
+            <input type="text" placeholder="inserisci la parola da bannare"  name="parolaBan">
+            
+            <label for="">testo</label>
+            <textarea name="testo" id="testo" cols="30" rows="10" placeholder="inserisci un testo" name="testoToBan"></textarea>
+
+            <button type="submit">Vai!</button>
+        </div>
+    </form>
+
+    
+</body>
+</html>
